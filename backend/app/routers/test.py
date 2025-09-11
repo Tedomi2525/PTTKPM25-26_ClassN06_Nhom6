7e1e2d78cdf26b6109f23f036e1cb82cd2304314
anhphap2412 <phapdamanh@gmail.com>
2025-09-11 23:15:15 +0700

    feat: add database connection

--- backend/app/routers/test.py
+++ backend/app/routers/test.py
@@ -1,8 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.schemas.test import InputData
+from app.database import SessionLocal
+from app.models.semester import Semester
 
 router = APIRouter()
 
-@router.post("/test")
-def test_post(payload: InputData):
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/get_test")
+def get_test(db=Depends(get_db)):
+    data = db.query(Semester).all()
+    return {"data": data} 
+
+@router.post("/post_test")
+def post_test(payload: InputData):
     return {"only_a": payload.a}
