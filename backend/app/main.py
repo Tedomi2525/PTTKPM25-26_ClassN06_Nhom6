34d699672bcb8f2ba46ba24d0b12b5aff73cf0cc
anhphap2412 <phapdamanh@gmail.com>
2025-09-23 08:15:19 +0700

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from app.routers import test, rooms, programs, users, teacher, program_courses,schedules, students, enrollments, course_class, auth, course_class, courses
-
+from fastapi.middleware.cors import CORSMiddleware
 # Tạo app
 app = FastAPI(
     title="QLDT API",
     description="Quản lý đào tạo - FastAPI backend",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # hoặc domain frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(test.router, prefix="/api")
@@ -18,6 +26,6 @@ app.include_router(program_courses.router, prefix="/api")
 app.include_router(students.router, prefix="/api")
 app.include_router(enrollments.router, prefix="/api")
 app.include_router(course_class.router, prefix="/api")
-app.include_router(auth.router, prefix="/api")
+app.include_router(auth.router, prefix="/auth")
 app.include_router(course_class.router, prefix="/api")
 app.include_router(courses.router, prefix="/api")
--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -11,7 +11,7 @@ def get_user_by_username(db: Session, username: str):
 
 def authenticate_user(db: Session, username: str, password: str):
     user = get_user_by_username(db, username)
-    if not user or not verify_password(password, user.hashed_password):
+    if not user or not verify_password(password, user.password):
         return None
     return user
 
